7b603e954abcc280868e2d2ce481016173924683
Z0SO <lautarorobales31@gmail.com>
2024-10-13 03:34:22 +0000

    falto una cosa del script

--- overthewire/descompresor.sh
+++ overthewire/descompresor.sh
@@ -15,10 +15,6 @@ trap ctrl_c INT
 first_file_name="test"
 
 
-
-# yo uso zl que es un alias hacia 7z l, lo mismo para zx que es 7z x
-
-
 descomprimido="$(7z l $first_file_name | tail -n 3 | head -n 1 | awk 'NF{print $NF}')"
 echo ''
 echo -e "[+] El primer archivo a descomprimir es: $first_file_name\n"
