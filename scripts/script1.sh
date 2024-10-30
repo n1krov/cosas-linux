2b835448d859c491f2a646b29c31cc9e1848d7fa
Z0SO <lautarorobales31@gmail.com>
2024-10-30 22:05:44 +0000

    [glob]-> eliminado bundle e ignorado

--- scripts/script1.sh
+++ scripts/script1.sh
@@ -53,19 +53,17 @@ function update_files(){
     # esta funcion se encarga de descargar el archivo bundle.js y formatearlo.
     
     clear
-
+    echo -e "${green}[*]${end} ${gray}Actualizando archivos...${end}\n"
+    sleep 2
 
     # el ! en bash es para negar, en este caso se comprueba si el archivo bundle.js no existe.
     if [ ! -f bundle.js ]; then
         
         echo -e "${red}[*]${end} ${gray}El archivo bundle.js no existe.${end}\n"
-        
         sleep 1
-
         echo -e "${green}[*]${end} ${gray}Descargando el archivo bundle.js...${end}\n"
 
         curl -s -X GET $url_file > bundle.js    # Se le podria qutar el -X GET, ya que por defecto es GET.
-
 
         # js-beautify es una herramienta que permite formatear el código javascript.
         # sponge es una herramienta que permite redirigir la salida de un comando a un archivo.
@@ -75,8 +73,10 @@ function update_files(){
         # la idea de este else es que si el archivo ya existe, se deba verificar una actulaziación del archivo.
         # ultilizando md5sum o sha256sum para verificar si el archivo ha sido modificado.
         echo -e "${green}[*] El archivo bundle.js ya existe.${end}\n"
+        sleep 1
+        echo -e "${green}[*]${end} ${gray}Comprobando actualizaciones...${end}\n"
 
-        sleep 2 # DEBUG
+        sleep 1 # DEBUG
 
         curl -s -X GET $url_file > bundle_temp.js
         js-beautify bundle_temp.js | sponge bundle_temp.js
@@ -88,8 +88,6 @@ function update_files(){
         echo -e "${green}[*]${end} ${gray}Valor original: ${end}${ylw}$original_value${end}"
         echo -e "${green}[*]${end} ${gray}Valor temporal: ${end}${green}$temp_value${end}\n"
 
-
-
         # la diferencia entre comparar las variables con "" y sin "" es que si se comparan con "", se comparan como cadenas de texto.
         # de lo contrario se comparan como números.
         if [ "$original_value" == "$temp_value" ] ; then
@@ -97,16 +95,10 @@ function update_files(){
             rm bundle_temp.js
         else
             echo -e "${green}[*]${end} ${gray}Actualizando archivo...${end}\n"
-
-
+            sleep 1
             rm bundle.js && mv bundle_temp.js bundle.js
         fi
-            
-
     fi
-
-
-
 
     # este if comprueba si el comando anterior se ha ejecutado correctamente.
     if [ $? -eq 0 ]; then
@@ -119,8 +111,6 @@ function update_files(){
     else
         echo -e "${red}[*]${end} ${gray}Error al formatear el archivo.${end}\n"
     fi
-
-
 }
 
 
@@ -139,7 +129,7 @@ function buscar_maquina(){
 
 
 
-# Nucleo del script -----------------------------
+#--------------------------- Nucleo del script -----------------------------
 
 # getopts es una función que permite pasar argumentos a un script, en este caso se pasan los argumentos -h y -m
 # m: indica que se espera un argumento después de la m, en este caso el argumento es el mensaje.
@@ -176,10 +166,9 @@ else
     buscar_maquina $mensaje
 fi
 
-
-echo -e "${ylw}[~] DEBUG: ${end}${gray}Sleep 10 segundos...${end}"
-sleep 10
+#--------------------------------- DEBUG ---------------------------------
 
 
+echo -e "${ylw}[~] DEBUG: ${end}${gray}Sleep 5 segundos...${end}"
+sleep 5
 tput cnorm    # muestra el cursor
-
