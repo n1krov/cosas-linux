148c92b6ea855d32b517ef0c67a0c81450590724
Z0SO <lautarorobales31@gmail.com>
2024-11-03 15:20:30 +0000

    edit script 1

--- scripts/script1.sh
+++ scripts/script1.sh
@@ -12,8 +12,6 @@ purple="\e[0;35m\033[1m"
 turquesa="\e[0;36m\033[1m"
 gray="\e[0;37m\033[1m"
 
-echo -e "${green}[*]${end} ${gray}Script de prueba${end}"
-
 function ctrl_c() {
     echo -e "\n${red}[*]${end} ${gray}SALIENDO...${end}"
     tput cnorm    # muestra el cursor
@@ -39,9 +37,13 @@ url_file="https://htbmachines.github.io/bundle.js"
 
 # Funcion de muestra del panel de ayuda
 function panel_help(){
-    echo -e "${green}[*]-------${end} ${gray}Panel de ayuda${end} ${green}-------[*]${end}"
+    echo -e "\n${green}[*]-------${end} ${gray}Panel de ayuda${end} ${green}-------[*]${end}\n"
 
-    echo -e "\t${green}[-m]${end} ${gray}Buscar una máquina con el nombre que se pasa como argumento.${end}"
+
+    echo -e "\t${green}[-m]${end} ${gray}Buscar una máquina por ${ylw}nombre${end}${gray}.${end}"
+
+    echo -e "\t${green}[-i]${end} ${gray}Buscar una máquina por ${ylw}Direccion IP${end}${gray}.${end}"
+
     echo -e "\t${green}[-u]${end} ${gray}Actualizar archivos.${end}"
     echo -e "\t${green}[-h]${end} ${gray}Mostrar panel de ayuda.${end}"
 }
@@ -120,12 +122,27 @@ function buscar_maquina(){
     clear
     
     mensaje="$*"
+
     echo -e "${green}[*]${end} ${gray}Buscando la máquina con el nombre -> ${end}${ylw}$mensaje${end}\n"
+
+
+    # con el comando cat se muestra el contenido del archivo bundle.js
+    #
+    cat bundle.js | awk "/name: \"${mensaje}\"/,/resuelta/" | grep -vE 'id|sku:|resuelta' | tr -d '"' | tr -d ',' | sed 's/^ */ [+] /'
 
 
 }
 
 
+function buscar_ip(){
+    # esta funcion se encarga de buscar una máquina en el archivo bundle.js.
+
+    clear
+    
+    ip_address="$1"   # $1 es el primer argumento que se pasa al script.
+
+    echo -e "${green}[*]${end} ${gray}Buscando la máquina con la dirección IP -> ${end}${ylw}$ip_address${end}\n"
+}
 
 
 
@@ -136,39 +153,56 @@ function buscar_maquina(){
 # h: indica que no se espera un argumento después de la h, en este caso se muestra el panel de ayuda.
 # arg: es la variable que se utiliza para almacenar los argumentos que se pasan al script.
 
-while getopts "m:uh" arg; do
+while getopts "m:uhi:" arg; do
     case $arg in
         m)
             var1=1
-            echo -e "${ylw}[~]${end}DEBUG: ${gray}Se ha pasado el argumento -m${end}"
+            echo -e "${ylw}[~]${end}DEBUG: ${gray}Se ha pasado el argumento -m${end}"; sleep 1
             # $OPTARG es la variable que almacena el argumento que se pasa al script. es una variable.
             mensaje=$OPTARG     
             ;;
         u)
-            echo -e "${ylw}[~]${end}DEBUG: ${gray}Se ha pasado el argumento -u${end}"
+            echo -e "${ylw}[~]${end}DEBUG: ${gray}Se ha pasado el argumento -u${end}"; sleep 1
             var1=2
+            ;;
+        i)
+            echo -e "${ylw}[~]${end}DEBUG: ${gray}Se ha pasado el argumento -i${end}"; sleep 1
+            ip_address=$OPTARG
+            var1=3
             ;;
 
         h)
+            var1=-1
             ;;
     esac
 done
-# si no se pasa ningún argumento al script, se muestra el panel de ayuda.
-# si se pasa el argumento -m, se muestra el mensaje que se pasa después del argumento -m.
-if [ $var1 -eq 0 ]; then
+
+
+
+if [ $var1 -eq -1 ]; then       # entra por aquí si se pasa el argumento -h
+    panel_help
+elif [ $var1 -eq 0 ]; then      # entra por aquí si no se pasa ningún argumento.
     clear
     echo -e "${red}[*]${end} ${gray}No se ha pasado ningún argumento.${end}\n"
+    sleep 2
     panel_help
-elif [ $var1 -eq 2 ]; then 
+elif [ $var1 -eq 1 ]; then    # entra por aquí si se pasa el argumento -m
+    buscar_maquina $mensaje
+elif [ $var1 -eq 2 ]; then      # entra por aquí si se pasa el argumento -u 
     # funcion update_files
     update_files
+elif [ $var1 -eq 3 ]; then      # entra por aquí si se pasa el argumento -i
+    buscar_ip $ip_address
 else
-    buscar_maquina $mensaje
+    clear
+    echo -e "${red}[*]${end} ${gray}Error en los argumentos.${end}\n"
+    sleep 2
+    panel_help
 fi
 
 #--------------------------------- DEBUG ---------------------------------
 
 
-echo -e "${ylw}[~] DEBUG: ${end}${gray}Sleep 5 segundos...${end}"
+echo -e "\n${ylw}[~] DEBUG: ${end}${gray}Sleep 5 segundos...${end}\n"
 sleep 5
 tput cnorm    # muestra el cursor
